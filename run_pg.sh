cd9ba38773a97b52e4249e87de9b5729aa1dda2b
arkohut <39525455+arkohut@users.noreply.github.com>
2025-01-31 10:57:47 +0800

    chore: add restart policy for postgresql docker

--- run_pg.sh
+++ run_pg.sh
@@ -1,5 +1,6 @@
 docker run -d \
     --name pensieve-pgvector \
+    --restart always \
     -p 5432:5432 \
     -e POSTGRES_PASSWORD=mysecretpassword \
     -v pensieve-pgdata:/var/lib/postgresql/data \
