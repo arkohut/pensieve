5384911e179d3e995f586a0e63dad6bf03bea05a
arkohut <39525455+arkohut@users.noreply.github.com>
2025-02-05 02:05:56 +0800

    feat: Add script to generate pensiev package configuration

---
+++ create_pensiev_toml.py
@@ -0,0 +1,18 @@
+import tomli
+import tomli_w
+
+def create_pensiev_toml():
+    # Read the original toml file
+    with open('pyproject.toml', 'rb') as f:
+        data = tomli.load(f)
+    
+    # Modify the project name and script name
+    data['project']['name'] = 'pensiev'
+    data['project']['scripts'] = {'pen': 'memos.commands:app'}
+    
+    # Write the modified data to the new file
+    with open('pyproject_pensiev.toml', 'wb') as f:
+        tomli_w.dump(data, f)
+
+if __name__ == '__main__':
+    create_pensiev_toml() 
