8c98e175d32e41078c794751f8541fa11a0c8349
arkohut <39525455+arkohut@users.noreply.github.com>
2025-04-03 00:16:15 +0800

    feat: Enhance Config.svelte with scroll detection and UI improvements
    - Added scroll detection functionality to change header style based on scroll position, improving user experience.
    - Updated header component to be sticky and responsive, enhancing visibility during scrolling.
    - Refactored layout padding in +layout.svelte for better alignment and visual consistency.
    - Included additional descriptive text for configuration options to aid user understanding.

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 <div class="min-h-screen bg-gradient">
 	<div class="min-h-screen">
 		<div class="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
-			<div class="relative pt-6 pb-16">
+			<div class="relative pt-0 pb-16">
 				<slot />
 			</div>
 		</div>
